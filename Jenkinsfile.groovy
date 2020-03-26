d530c8ae9b1d024d74a1d0abd081b049a2b6f807
Pankaj <pankajakhade@gmail.com>
2020-03-26 11:25:44 +0530

    modified Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,4 +13,16 @@ node{
         def buildCauses = currentBuild.getBuildCauses()
         print buildCauses
     }
+    stage('Get EC2 Instances'){
+        withCredentials([string(credentialsId: 'aws-secret-id', variable: 'awsCredentialsId')]) {
+            withCredentials([$class           : 'AmazonWebServicesCredentialsBinding',
+                          accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                          credentialsId    : awsCredentialsId,
+                          secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'])
+                {
+                  sh 'aws ec2 describe-instances --filters "Name=instance-type,Values=t2.micro" --query "Reservations[].Instances[].InstanceId"'
+                }
+      }
+    }
 }
+
