13d5f08b4fb20a26dd7ef31a4d9723d61e56fac5
pankajakhade <44667391+pankajakhade@users.noreply.github.com>
2020-08-14 18:22:30 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,19 +1,18 @@
 #!/usr/bin/groovy
 
-branch_list = ['master']
-if (env.BRANCH_NAME in branch_list){
-    node{
-        stage('SCM'){
-            checkout scm
-        }    
-        /*stage('Read File'){
-            def my_file = env.WORKSPACE + '/textfile'
-            File file = new File(my_file)
-            print file.text
-        }*/
-        stage('Get Buildcause'){
-            def buildCauses = currentBuild.getBuildCauses()
-            print buildCauses
-        }
+
+node{
+    stage('SCM'){
+        checkout scm
+    }    
+    /*stage('Read File'){
+        def my_file = env.WORKSPACE + '/textfile'
+        File file = new File(my_file)
+        print file.text
+    }*/
+    stage('Get Buildcause'){
+        def buildCauses = currentBuild.getBuildCauses()
+        print buildCauses
     }
 }
+
